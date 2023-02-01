1505c200a80df7e8d4dc5317ab9310a688846f8e
danielqiu <qiuwf@shanhs.com>
2023-02-01 16:19:13 +0800

    change

--- src/pages/index/components/IndexHeader.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import BaseComponent from "@/base/BaseComponent";
-
-export default class IndexHeader extends BaseComponent {
-
-  render() {
-    return <div></div>;
-  }
-}
--- src/pages/index/main/index_main.tsx
+++ src/pages/index/main/index_main.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import autobind from "@/HOC/autobind";
-import { View, Text, Image } from "@tarojs/components";
+import { View, Text } from "@tarojs/components";
 import {AppStore} from '@/store/modules/app'
 import "./index.less";
-import TopBackground from '@/assets/images/index_top-bg.png'
 import BaseComponent from "@/base/BaseComponent";
-import IndexStatusBar from "../components/IndexStatusBar";
 
 
  @autobind('AppStore')
@@ -15,10 +13,10 @@ export default class Index extends BaseComponent<{}, PageIndexState, AppStore> {
   }
 
   UNSAFE_componentWillMount() {
-    this.props.navbar?.show({
-      transparent: true,
-      renderLeft: () => <IndexStatusBar />
-    })
+    // this.props.navbar?.show({
+    //   transparent: true,
+    //   renderLeft: () => <IndexStatusBar />
+    // })
   }
 
   store: AppStore;
@@ -37,7 +35,6 @@ export default class Index extends BaseComponent<{}, PageIndexState, AppStore> {
     // const { state } = this.store
     return (
       <View className="index">
-        <Image src={TopBackground} className={'top-bg'} />
         <Text>这是Page Index的内容！！</Text>
       </View>
     );
